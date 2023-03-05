e4629eb964621bac78f59c54933daaeb257369ca
Seifeldin-Ahmed <120275931+Seifeldin-Ahmed@users.noreply.github.com>
2023-03-05 03:40:16 +0200

    Delete Temperature Sensor directory

--- Temperature Sensor/Temperature Sensor/Debug/HAL/LED/LED_prg.d
+++
@@ -1,9 +0,0 @@
-HAL/LED/LED_prg.d HAL/LED/LED_prg.o: ../HAL/LED/LED_prg.c \
- ../HAL/LED/../../LIB/STD_Types.h ../HAL/LED/LED_int.h \
- ../HAL/LED/../../MCAL/DIO/DIO_int.h
-
-../HAL/LED/../../LIB/STD_Types.h:
-
-../HAL/LED/LED_int.h:
-
-../HAL/LED/../../MCAL/DIO/DIO_int.h:
